db9c0e9df09cbc373f371233fd159acfd44391c4
alex <alexandry.augustin@gmail.com>
2023-11-10 13:27:03 +0100

    update

--- cookbook/python/third-party-libraries/scipy/pandas/core/pandas_to_csv.py
+++ cookbook/python/third-party-libraries/scipy/pandas/core/pandas_to_csv.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 if __name__ == '__main__':
-	data=pd.read_csv("../datasets/data.csv", sep=",") #, dtype={'rating': np.float32}
-	data.to_csv('out.csv', sep=',', index=False)#, columns=[""]) #no write row index/name
+
+	data = pd.read_csv('../datasets/data.csv', sep=',') #, dtype={'rating': np.float32}
+	data.to_csv('out.csv', sep=',', index=False)#, columns=['']) #no write row index/name
