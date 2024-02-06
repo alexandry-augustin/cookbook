d95ba471b87d96088331cb5b63a13f6e160236a8
alex <alexandry.augustin@gmail.com>
2024-02-06 17:49:13 +0100

    update

---
+++ cookbook/htmx/select_change/server.py
@@ -0,0 +1,17 @@
+from random import randrange
+
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/fake', methods=['GET'])
+def fake():
+    print(request.args)
+    return "Done"
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=8000, debug=True)
