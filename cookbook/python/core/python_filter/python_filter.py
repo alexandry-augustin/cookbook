d72078d4c9aea090d5d597b135d330d08d8b9f54
alex <alexandry.augustin@gmail.com>
2024-03-18 13:54:39 +0100

    update

--- cookbook/python/core/python_filter/python_filter.py
+++ cookbook/python/core/python_filter/python_filter.py
@@ -1,5 +1,7 @@
 if __name__ == '__main__':
 
     l = list(range(5))
+    # Return a new list with only even values
     assert(list(filter(lambda x: x%2 == 0, l)) == [ 0, 2, 4 ])
+    # Return a new list with only odd values
     assert(list(filter(lambda x: x%2 != 0, l)) == [ 1, 3 ])
