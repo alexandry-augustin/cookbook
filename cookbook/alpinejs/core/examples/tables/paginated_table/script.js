63780e23eae597e3ca084b6fa1268e45f792fdd7
alex <alexandry.augustin@gmail.com>
2024-06-03 17:54:20 +0200

    update

--- cookbook/alpinejs/core/examples/tables/paginated_table/script.js
+++ cookbook/alpinejs/core/examples/tables/paginated_table/script.js
@@ -1,19 +1,19 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data('catData', () => ({
-      cats:null,
-      sortCol:null,
-      sortAsc:false,
-      pageSize:4,
-      curPage:1,
+    Alpine.data('data', () => ({
+      items: null,
+      sortCol: null,
+      sortAsc: false,
+      pageSize: 4,
+      curPage: 1,
       async init() {
         let resp = await fetch('https://www.raymondcamden.com/.netlify/functions/get-cats');
         // Add an ID value
         let data = await resp.json();
         data.forEach((d,i) => d.id = i);
-        this.cats = data;
+        this.items = data;
       },
       nextPage() {
-        if((this.curPage * this.pageSize) < this.cats.length) this.curPage++;
+        if((this.curPage * this.pageSize) < this.items.length) this.curPage++;
       },
       previousPage() {
         if(this.curPage > 1) this.curPage--;
@@ -21,15 +21,15 @@ document.addEventListener('alpine:init', () => {
       sort(col) {
         if(this.sortCol === col) this.sortAsc = !this.sortAsc;
         this.sortCol = col;
-        this.cats.sort((a, b) => {
+        this.items.sort((a, b) => {
           if(a[this.sortCol] < b[this.sortCol]) return this.sortAsc?1:-1;
           if(a[this.sortCol] > b[this.sortCol]) return this.sortAsc?-1:1;
           return 0;
         });
       },
-      get pagedCats() {
-        if(this.cats) {
-          return this.cats.filter((row, index) => {
+      get pagedItems() {
+        if(this.items) {
+          return this.items.filter((row, index) => {
             let start = (this.curPage-1)*this.pageSize;
             let end = this.curPage*this.pageSize;
             if(index >= start && index < end) return true;
