8154014626690d4d26fe532581b7bec14c3cdfb5
alex <alexandry.augustin@gmail.com>
2024-10-21 12:09:37 +0200

    update

--- cookbook/python/stdlib/python_math/python_math.py
+++ cookbook/python/stdlib/python_math/python_math.py
@@ -1,7 +1,21 @@
 import math
 
+def test_exp_log():
+
+	assert(math.exp(1) == math.e)
+	#assert(math.exp(math.log(5, math.e)) == 5) #! rounding error
+	assert(math.log(math.exp(5), math.e) == 5)
+	assert(math.log(math.e) == 1)
+	assert(math.log(1) == 0)
+
 def test_isclose():
 	"""
+
+	math.isclose(a, b, rel_tol, abs_tol)
+	
+	rel_tol: (Optional) The relative tolerance. It is the maximum allowed difference between value a and b. Default value is 1e-09
+	abs_tol: (Optional) The minimum absolute tolerance. It is used to compare values near 0. The value must be at least 0
+
 	Equivalent function (https://peps.python.org/pep-0485/#proposed-implementation):
 
 		def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
@@ -15,11 +29,6 @@ def test_isclose():
 if __name__ == '__main__':
 
 	assert(math.factorial(3) == 6)
-	assert(math.exp(1) == math.e)
-	#assert(math.exp(math.log(5, math.e)) == 5) #! rounding error
-	assert(math.log(math.exp(5), math.e) == 5)
-	assert(math.log(math.e) == 1)
-	assert(math.log(1) == 0)
 	assert(math.pow(2, 4) == 16)
 	assert(math.pow(2, 4) == 2**4)
 	assert(math.sqrt(16) == 4)
@@ -29,4 +38,5 @@ if __name__ == '__main__':
 	print(format(n, '02x')) #02: at least 2 digits x:lower-case hex
 	print('%02x' % n)
 
+	test_exp_log()
 	test_isclose()
--- cookbook/python/third-party/scipy/pandas/core/pandas_dataframe.py
+++ cookbook/python/third-party/scipy/pandas/core/pandas_dataframe.py
@@ -4,50 +4,81 @@ Selecting a single column from the DataFrame will return a Series object.
 
 import pandas as pd
 import numpy as np
-#----------------------------------------------------------
+
 def from_dictionary():
-	dic={"City":["Paris", "London"], "inhabitants":[2244000, 8630000]}
-	#or
-	dic=[{"City":"Paris", "inhabitants":2244000},
-		{"City":"London", "inhabitants":8630000}]
-	data=pd.DataFrame(dic)
-#----------------------------------------------------------
+
+	data = {
+		"City": [
+			"Paris", 
+			"London"
+		], 
+		"inhabitants": [
+			2244000, 
+			8630000
+		]
+	}
+
+	# or
+	
+	data = [
+		{
+			"City": "Paris", 
+			"inhabitants": 2244000
+		},
+		{
+			"City": "London", 
+			"inhabitants": 8630000
+		}
+	]
+
+	df = pd.DataFrame(data)
+
+def from_list():
+
+	data = [
+		[1, 2], 
+		[3, 4]
+	]
+
+	df = pd.DataFrame(data, columns=[ 'A', 'B' ])
+
 if __name__ == '__main__':
+
 	data=pd.read_csv("../datasets/cities.csv", sep=";")
 	#--------------------------------------------------
 	# get fields' header
 	#--------------------------------------------------
-	print list(data.columns.values)
+	print(list(data.columns.values))
 	#--------------------------------------------------
 #	print list(data.columns.values) #get column names
 	#--------------------------------------------------
 	# unique
 	#--------------------------------------------------
-	print data.Inhabitants.unique() #array of unique values
-	print data.Inhabitants.nunique() #number of unique elements
+	print(data.Inhabitants.unique()) #array of unique values
+	print(data.Inhabitants.nunique()) #number of unique elements
 	#--------------------------------------------------
 	# remove constant columns
 	#--------------------------------------------------
-	print data.loc[:, (data!=data.ix[0]).any()]
-	print data.loc[:, data.apply(pd.Series.nunique)!=1]
+	print(data.loc[:, (data!=data.ix[0]).any()])
+	print(data.loc[:, data.apply(pd.Series.nunique)!=1])
 
-#	print data
-#	print data["City"]
+#	print(data)
+#	print(data["City"])
 
-#	print data.mean()
-#	print data.std() #unbiased: =1/(n-1)*sum()
+#	print(data.mean())
+#	print(data.std() #unbiased: =1/(n-1)*sum())
 
-#	print data.City
-#	print data.City.value_counts() #return list of unique element with their frequency
-#	print np.unique(data.City.values)
+#	print(data.City)
+#	print(data.City.value_counts()) #return list of unique element with their frequency
+#	print(np.unique(data.City.values))
 	#--------------------------------------------------
 	# Apply to each element
 	#--------------------------------------------------
-	df = pd.DataFrame({"A":[1, 2]})
-	df["A"]=df["A"].apply(np.sqrt)
+	df = pd.DataFrame({ "A": [1, 2] })
+	df["A"] = df["A"].apply(np.sqrt)
 
 	df = pd.DataFrame({"A":[1.398, 2.857]})
-	df["A"]=df["A"].apply(lambda x: float("%.1f"%x))
+	df["A"] = df["A"].apply(lambda x: float("%.1f"%x))
 	#--------------------------------------------------
 	# as_matrix()
 	#--------------------------------------------------
@@ -55,9 +86,9 @@ if __name__ == '__main__':
 	#--------------------------------------------------
 	# append new row
 	#--------------------------------------------------
-	df = pd.DataFrame([[1, 2], [3, 4]], columns=list('AB'))
-	df2 = pd.DataFrame([[5, 6], [7, 8]], columns=list('AB'))
-	df=df.append(df2)
+	df = pd.DataFrame([[1, 2], [3, 4]], columns=[ 'A', 'B' ])
+	df2 = pd.DataFrame([[5, 6], [7, 8]], columns=[ 'A', 'B' ])
+	df = df.append(df2)
 	#print df
 	# A  B
 	# 1  2
@@ -68,9 +99,9 @@ if __name__ == '__main__':
 	#--------------------------------------------------
 	# append new column
 	#--------------------------------------------------
-	df = pd.DataFrame([[1, 2], [3, 4]], columns=list('AB'))
-	df["A-B"]=pd.DataFrame(df["B"]-df["A"])
-	print df
+	df = pd.DataFrame([[1, 2], [3, 4]], columns=[ 'A', 'B' ])
+	df["A-B"] = pd.DataFrame(df["B"]-df["A"])
+	print(df)
 	#pd.concate([df, df2], axis=1)
 	#--------------------------------------------------
 	# check if column exist
@@ -82,12 +113,12 @@ if __name__ == '__main__':
 	#--------------------------------------------------
 	# to_csv()
 	#--------------------------------------------------
-#	df.to_csv("out.csv", index=False, sep=",")
+	# df.to_csv("out.csv", index=False, sep=",")
 	#--------------------------------------------------
 	# dataframe from scratch
 	#--------------------------------------------------
-	columns = ['A', 'B', 'C']
-	index=range(0,len(columns))
-	df=pd.DataFrame(index=index, columns=columns)
-	df["D"]=np.nan #add new field
-	print df.head()
+	columns = [ 'A', 'B', 'C' ]
+	index = range(0, len(columns))
+	df = pd.DataFrame(index=index, columns=columns)
+	df["D"] = np.nan # add new field
+	print(df.head())
