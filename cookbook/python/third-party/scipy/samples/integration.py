d1c534b266342ef51c15e292b43fab10968084c7
alex <alexandry.augustin@gmail.com>
2024-10-24 16:53:17 +0200

    update

--- cookbook/python/third-party/scipy/samples/integration.py
+++ cookbook/python/third-party/scipy/samples/integration.py
@@ -1,9 +1,10 @@
-#http://stackoverflow.com/questions/13320262/calculating-the-area-under-a-curve-given-a-set-of-coordinates-without-knowing-t
+# http://stackoverflow.com/questions/13320262/calculating-the-area-under-a-curve-given-a-set-of-coordinates-without-knowing-t
 import numpy as np
 from scipy.integrate import simps
 #import scipy as sp
 
 if __name__=="__main__":
+
 	y = np.array([5, 20, 4, 18, 19, 18, 7, 4])
 
 	# Compute the area using the composite trapezoidal rule.
