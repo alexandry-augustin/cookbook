0be739ec3ba3dd27aeac79cb0145b50a931865c3
alex <alexandry.augustin@gmail.com>
2025-01-22 10:02:23 +0100

    antora/multi-repos-2 added

---
+++ cookbook/python/third-party/cookiecutter/cookicutters/antora/multi-repos-2/hooks/post_gen_project.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Site repo
+
+git init
+git config user.name {{cookiecutter.git_username}}
+git config user.email {{cookiecutter.git_email}}
+
+# Component "{{cookiecutter.component_name}}" repo
+
+cd ./git/{{cookiecutter.component_name}}
+
+git init
+git config user.name {{cookiecutter.git_username}}
+git config user.email {{cookiecutter.git_email}}
+git init && git commit --allow-empty -m "Initial commit"
+
+ln -s ../../{{cookiecutter.component_name}}/antora.yml
+ln -s ../../{{cookiecutter.component_name}}/modules/
---
+++ cookbook/python/third-party/cookiecutter/cookicutters/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source .venv/bin/activate
+
+# cookiecutter ./antora/mono-repo/
+# cookiecutter ./antora/mono-repo-2/
+# cookiecutter ./antora/multi-repos/
+cookiecutter ./antora/multi-repos-2/
